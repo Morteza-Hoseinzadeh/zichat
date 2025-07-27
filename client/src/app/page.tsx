77074b44404389ed09f0f4188eeb9e8e14e0cd55
Morteza-hosseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-07-27 14:31:50 +0330

    Clean up codes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useThemeMode } from '@/utils/hooks/useThemeMode';
 
 export default function page() {
-  const { toggleTheme, isDarkMode } = useThemeMode();
-
-  return (
-    <div>
-      <button onClick={toggleTheme}>{isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}</button>
-    </div>
-  );
+  return <div></div>;
 }
