7a998a46587b02981fcaa3b56ee1039db1ac66a7
Morteza-hosseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-08-03 17:32:13 +0330

    Develop get cryptoes price API route

---
+++ client/server/route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// User side routes
+const crypto_file = require('./routes/cryptoes/route');
+
+// Client side routes
+router.use('/crypto/prices', crypto_file);
+
+module.exports = { router };
